f1f79b25c5cdeaad1db6722c26668ff949cb34ba
mannynav <mannynav7794@gmail.com>
2024-08-01 18:35:57 +0100

    Member Contracts

---
+++ GymProject/Contracts/MemberContract.cs
@@ -0,0 +1,14 @@
+﻿namespace GymProject.Contracts;
+
+public record class MemberContract(
+
+    int Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string PhoneNumber,
+    int Height,
+    int Weight,
+    string Goal,
+    DateOnly JoiningDate
+);
--- GymProject/Program.cs
+++ GymProject/Program.cs
@@ -1,5 +1,40 @@
+using System.Diagnostics.Contracts;
+using GymProject.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
+
+
+List<MemberContract> contracts = [
+
+    new (
+        1,
+        "John",
+        "Doe",
+        "johndoe@gmail.com",
+        "1234567890",
+        180,
+        80,
+        "Lose Weight",
+        new DateOnly(2021, 10, 10)
+    ),
+    new (
+        2,
+        "Beth",
+        "Doe",
+        "bethdoe@gmail.com",
+        "0987654321",
+        120,
+        60,
+        "Add Weight",
+        new DateOnly(2022, 10, 10)
+    )
+
+];
+
+app.MapGet("members", () => contracts);
+
 
 app.MapGet("/", () => "Hello World!");
 
